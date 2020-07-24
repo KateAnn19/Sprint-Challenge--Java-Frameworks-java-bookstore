a66d8c1a74e7cc30b68beee739b45769e1597f53
Kate McGee <mcgeehee@gmail.com>
2020-07-24 11:12:57 -0600

    added questions

--- bookstore/src/main/java/com/lambdaschool/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/lambdaschool/bookstore/BookstoreApplication.java
@@ -57,3 +57,25 @@ public class BookstoreApplication
         }
     }
 }
+
+
+//    1) Can you explain exception handling in your application?
+//    Spring has built in error handling so we add some config and custom error handling files
+//    and connect our services to the custom error handling exceptions in order to overrride the
+//    default  messages
+//    Can you explain your user authentication flow?
+//    We have to add a bunch of authentication config files which authorize a user and then
+//    authenticate the user. We are using auth2 to do our security and use environment variables in order
+//    send and receive a token which is crypted
+
+//    Can you show me your unit tests and describe how they work?
+//    there are tests for the bookservice and bookcontroller. the book service uses the seed data
+//    already there and book controller builds its' own data not relying on seed data
+//    the tests in book controller mock up data. the tests in book service check the expected outcome
+//    against the data already there
+
+//    Can you show how you deployed your application to a cloud service with a persistent database?
+//    I used Heroku to deploy my application. I had to add a new config file and some application properties
+//    as well as a Heroku plugin to my POM file and postgresSql dependency. I deployed to Heroku
+//    using the CLI and then connected the application in my POM file using the name of the application
+//    I used in created my new Heroku application 
